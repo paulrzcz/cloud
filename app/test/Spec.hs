78ff817e324a434b655d3f9ae284ad0d9832b50e
Pavel <paul@paulrz.cz>
2016-10-20 23:27:05 +0200

    Tests of random generator

--- app/test/Spec.hs
+++ app/test/Spec.hs
@@ -1,12 +1,24 @@
 module Main where
 
 import           Test.Hspec
-import           Test.QuickCheck
+import           Test.QuickCheck.Monadic
+
+import           Control.Monad.Trans.Class
 
 import           Message
+import           Random
+import           System.Random.Mersenne.Pure64
 
 main :: IO ()
 main = hspec $ do
   describe "Result update tests" $ do
     it "Adding the first message with 0.5" $
-      updateResult initialState (Message 0.5) `shouldBe` (ResultState 1 0.5)
+      updateResult initialState (Payload 0.5) `shouldBe` (ResultState 1 0.5)
+    it "Adding the second message with 0.5" $
+      updateResult (ResultState 1 0.5) (Payload 0.5) `shouldBe` (ResultState 2 1.5)
+  describe "Random generator property test" $ do
+    it "No zero components in random generator (probabilistic)" $ do
+      monadicIO $ do
+        rng <- lift newPureMT
+        let (Payload x, _) = getRandomMessage rng
+        return $ x > 0.0
